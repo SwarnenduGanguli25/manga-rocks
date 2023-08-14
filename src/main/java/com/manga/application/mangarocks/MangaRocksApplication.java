ccebef4791009c097ebbe7725798fb67f9d9debd
SwarnenduGanguli25 <swarnenduganguli@gmail.com>
2023-08-14 11:05:27 +0530

    manga-rocks application

---
+++ src/main/java/com/manga/application/mangarocks/MangaRocksApplication.java
@@ -0,0 +1,13 @@
+package com.manga.application.mangarocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaRocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaRocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manga/application/mangarocks/MangaRocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manga.application.mangarocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaRocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
