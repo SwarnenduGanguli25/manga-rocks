2a0faa2b1c6dc8608dbc1b47bfbf9be56770cc99
SwarnenduGanguli25 <swarnenduganguli@gmail.com>
2023-08-14 11:49:30 +0530

    Adding logs in Application file

--- src/main/java/com/manga/application/mangarocks/MangaRocksApplication.java
+++ src/main/java/com/manga/application/mangarocks/MangaRocksApplication.java
@@ -1,13 +1,20 @@
 package com.manga.application.mangarocks;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
+@Slf4j
 public class MangaRocksApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting the Server....");
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
 		SpringApplication.run(MangaRocksApplication.class, args);
+		log.info("Server is up!! with timezone {}", TimeZone.getDefault());
 	}
 
 }
